32e40fc91cd0b8d809f8da51aa73c76ddb132fd9
GuhzeraDev <109148206+GuhzeraDev@users.noreply.github.com>
2023-05-31 15:41:58 -0300

    Update exercicio68.py

--- exercicio68.py
+++ exercicio68.py
@@ -1,27 +1,33 @@
+'''
+Exercício Python 68: Faça um programa que jogue par ou ímpar com o computador.
+O jogo só será interrompido quando o jogador perder, mostrando o total de vitórias
+consecutivas que ele conquistou no final do jogo.
+'''
+
 from random import randint
 n = 0
 cont = 0
 escolha = 'P'
-
 
 print('=-='*10)
 print('Vamos jogar par ou impar')
 print('=-='*10)
 while True:
     n = int(input('Diga um valor: '))
-    escolha = str(input('Par ou Impar? [P/I] ')).upper().strip()
+    escolha = str(input('Par ou Impar? [P/I] ')).upper().strip()[0]
+    while escolha not in 'PI':
+        escolha = str(input('Par ou Impar? [P/I] ')).upper().strip()[0]
     ale = randint(0, 10)
     soma = n + ale
     print(f'Você colocou {n} e eu coloquei {ale} dando {soma} que é ', end='')
+    print('Deu par' if soma % 2 == 0 else 'Deu Impar')
     if soma % 2 == 0:
-        print('Par')
         if escolha == 'P':
             print('Você venceu vamos mais uma vez.')
         else:
             print('Eu ganhei!!')
             break
     else:
-        print('Impar')
         if escolha == 'I':
             print('Você venceu vamos mais uma vez.')
         else:
@@ -29,3 +35,6 @@ while True:
             break
     cont += 1
 print(f'Você ganhou {cont} vezes antes de perder!!')
+
+
+
