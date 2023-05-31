67f2c64cc167d84b458ae8f5cd3ff5d0a282fcd5
GuhzeraDev <109148206+GuhzeraDev@users.noreply.github.com>
2023-05-31 15:42:40 -0300

    Update exercicio70.py

--- exercicio70.py
+++ exercicio70.py
@@ -1,23 +1,33 @@
-total = 0
-menor = 99999999
+'''
+Exercício Python 70: Crie um programa que leia o nome e o preço de vários produtos.
+O programa deverá perguntar se o usuário vai continuar ou não. No final, mostre:
+
+A) qual é o total gasto na compra.
+
+B) quantos produtos custam mais de R$1000.
+
+C) qual é o nome do produto mais barato.
+
+'''
+
+total = menor = caro = 0
 nomeBarato = ''
-caro = 0
 
 while True:
-
     nome = str(input('Digite o nome do produto: ')).strip()
-    preço = int(input('Qual o preço deste produto? R$'))
+    preço = float(input('Qual o preço deste produto? R$'))
+    if total == 0 or preço < menor:
+        menor = preço
+        nomeBarato = nome
     total += preço
     if preço > 1000:
         caro += 1
-    if preço < menor:
-        menor = preço
-        nomeBarato = nome
-
-    pergunta = str(input('Vai continuar [S/N]: ')).upper().strip()
+    pergunta = ' '
+    while pergunta not in 'SN':
+        pergunta = str(input('Vai continuar [S/N]: ')).upper().strip()[0]
     if pergunta == 'N':
         break
-print(f'''(A) O total gasto na compra foi R${total}.
-(B) {caro} produtos custam mais de R$1000.
-(C) O produto mais barato é o {nomeBarato} que custa R${menor}
+print(f'''(A) O total gasto na compra foi R${total:.2f}
+(B) {caro} produtos custam mais de R$1000
+(C) O produto mais barato é o {nomeBarato} que custa R${menor:.2f}
 ''')
