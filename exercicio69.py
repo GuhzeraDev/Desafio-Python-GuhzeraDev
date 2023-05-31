d8376fb421317d15b9bc7844e3a94b8d90c35dd6
GuhzeraDev <109148206+GuhzeraDev@users.noreply.github.com>
2023-05-31 15:42:19 -0300

    Update exercicio69.py

--- exercicio69.py
+++ exercicio69.py
@@ -1,24 +1,34 @@
+'''
+Exercício Python 69: Crie um programa que leia a idade e o sexo de várias pessoas.
+A cada pessoa cadastrada, o programa deverá perguntar se o usuário quer ou não continuar. No final, mostre:
+
+A) quantas pessoas tem mais de 18 anos.
+B) quantos homens foram cadastrados.
+C) quantas mulheres tem menos de 20 anos.
+'''
 adulto = 0
 homen = 0
 mulher = 0
-idade = 0
-sexo = 'M'
 
 while True:
+    print('-=-' * 20)
     idade = int(input('Digite sua idade: '))
-    sexo = str(input('Digite seu sexo [H/M]: ')).upper().strip()
-
+    sexo = ' '
+    while sexo not in 'MF':
+        sexo = str(input('Digite seu sexo [M/F]: ')).upper().strip()[0]
     if idade > 18:
         adulto += 1
-    if sexo == 'H':
+    if sexo == 'M':
         homen += 1
-    if sexo == 'M' and idade < 20:
+    if sexo == 'F' and idade < 20:
         mulher += 1
-    pergunta = str(input('Deseja continuar? [S/N]: ')).upper().strip()
+    pergunta = ' '
+    while pergunta not in 'SN':
+        pergunta = str(input('Deseja continuar? [S/N]: ')).upper().strip()[0]
     if pergunta == 'N':
         break
-
+print('-=-' * 20)
 print(f'''(A) {adulto} pessoas tem mais de 18 anos.
-(B) {homen} pessoas cadastradas são homens
-(C) {mulher} pessoas cadastradas são mulheres e tem menos de 20 anos
+(B) {homen} pessoas são homens
+(C) {mulher} pessoas são mulheres e tem menos de 20 anos
 ''')
