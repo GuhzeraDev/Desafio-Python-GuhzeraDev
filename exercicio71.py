b6813d32d63ece0e3bbd6990cb27527dca3100c5
GuhzeraDev <109148206+GuhzeraDev@users.noreply.github.com>
2023-05-31 15:42:58 -0300

    Update exercicio71.py

--- exercicio71.py
+++ exercicio71.py
@@ -1,3 +1,35 @@
+'''
+Exercício Python 071: Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues. OBS:
+
+considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
+
+'''
+
+'''
+print('Banco Teste')
+valor = int(input('Digite o valor que quer sacar R$: '))
+total = valor
+ced = 50
+totced = 0
+
+while True:
+    if total >= ced:
+        total -= ced
+        totced += 1
+    else:
+        if totced > 0:
+            print(f'Total de {totced} cedulas de R${ced}')
+        elif ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totced = 0
+        if total == 0:
+            break
+'''
+
 cedula50 = 0
 cedula20 = 0
 cedula10 = 0
@@ -17,11 +49,14 @@ while True:
     cedula1 = resto // 1
     break
 
-print(f'''Para sacar o valor de {valor} foram ultilizadas:
-{cedula50} notas de R$50
-{cedula20} notas de R$20
-{cedula10} notas de R$10
-{cedula1} notas de R$1
-''')
+print(f'Para sacar o valor de {valor} foram ultilizadas: ')
+if cedula50 > 0:
+    print(f'{cedula50} notas de R$50')
+if cedula20 > 0:
+    print(f'{cedula20} notas de R$20')
+if cedula10 > 0:
+    print(f'{cedula10} notas de R$10')
+if cedula1 > 0:
+    print(f'{cedula1} notas de R$1')
 print('Volte sempre ao Banco do Guh! Tenha um otimo dia!')
 
